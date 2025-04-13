3177fc8fa91772417f07f806b09009818e8271a8
Grigoriy Panin <llchh@yahoo.com>
2025-04-13 22:49:41 +0700

    Debounce

--- Debounce/main.go
+++ Debounce/main.go
@@ -1,1 +1,38 @@
-package Debounce
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Circuit func(ctx context.Context) (string, error)
+
+func Debounce(circuit Circuit, d time.Duration) Circuit {
+	var threshold time.Time
+	var result string
+	var err error
+	var m sync.Mutex
+	return func(ctx context.Context) (string, error) {
+		m.Lock()
+		defer func() {
+			threshold = time.Now().Add(d)
+			m.Unlock()
+		}()
+		if time.Now().Before(threshold) {
+			return result, err
+		}
+		return circuit(ctx)
+	}
+}
+
+func main() {
+	res := Debounce(func(ctx context.Context) (string, error) {
+		return "fetching data...", nil
+	}, time.Duration(10*time.Microsecond))
+	for i := 0; i <= 10; i++ {
+		fmt.Println(res(context.TODO()))
+	}
+
+}
